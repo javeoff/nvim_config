5b6093dcaf188c0d7065fc67a5782c8d4f29ea94
bondarev <javeoff@yandex.ru>
2024-07-06 22:02:00 +0400

    feat: update decrypion key

--- scripts/decrypt_key.sh
+++ scripts/decrypt_key.sh
@@ -2,7 +2,7 @@
 
 read -p "Enter the prefix for the files to decrypt (default is id_rsa): " PREFIX
 PREFIX=${PREFIX:-id_rsa}
-ENCRYPTED_KEY_PATH=./.${PREFIX}
+ENCRYPTED_KEY_PATH=~/.git/${PREFIX}
 
 if [ ! -f "$ENCRYPTED_KEY_PATH" ]; then
   echo "Encrypted file not found at $ENCRYPTED_KEY_PATH"
@@ -21,3 +21,14 @@ else
   echo "Your decrypted SSH private key:"
   echo "$DECRYPTED_KEY"
 fi
+
+read -p "Do you want to generate the public key from the private key? (y/n): " GEN_PUB_KEY
+if [ "$GEN_PUB_KEY" = "y" ]; then
+  TEMP_PRIV_KEY=$(mktemp)
+  echo "$DECRYPTED_KEY" > "$TEMP_PRIV_KEY"
+  PUB_KEY=$(ssh-keygen -y -f "$TEMP_PRIV_KEY")
+  rm "$TEMP_PRIV_KEY"
+
+  read -p "Do you want to save the private key to ~/.ssh/id_rsa? (y/n): " SAVE_PRIV_KEY
+  if [ "$SAVE_PRIV_KEY" = "
+
