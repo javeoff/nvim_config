bb54da066ea1807800b5754801d56f5b38abfd34
bondarev <javeoff@yandex.ru>
2024-07-06 18:02:49 +0400

    feat: update config

--- scripts/macos-install.sh
+++ scripts/macos-install.sh
@@ -1,4 +1,8 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip-components 1 -C homebrew
+eval "$(homebrew/bin/brew shellenv)"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
+
 brew install neovim
 mkdir -p ~/.config
 mkdir -p ~/.config/nvim
