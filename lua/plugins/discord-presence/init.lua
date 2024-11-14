2d91558ac789835afa6dbb9ecbca89e430a435d7
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-11-14 11:29:53 +0700

    feat: update discord presence plugin configuration

--- lua/plugins/discord-presence/init.lua
+++ lua/plugins/discord-presence/init.lua
@@ -1,28 +1,28 @@
 return {
-    'IogaMaster/neocord',
-    event = "VeryLazy",
-		enabled = true,
-    config = function()
-	    require("neocord").setup({
-		    logo                = "auto",                     
-		    logo_tooltip        = nil,                        
-		    main_image          = "language",                 
-		    client_id           = "1157438221865717891",      
-		    log_level           = nil,                        
-		    debounce_timeout    = 10,                         
-		    blacklist           = {},                         
-		    file_assets         = {},                         
-		    show_time           = true,                       
-		    global_timer        = false,                      
+	'IogaMaster/neocord',
+	event = "VeryLazy",
+	enabled = true,
+	config = function()
+		require("neocord").setup({
+			logo                = "auto",
+			logo_tooltip        = nil,
+			main_image          = "language",
+			client_id           = "1157438221865717891",
+			log_level           = nil,
+			debounce_timeout    = 10,
+			blacklist           = {},
+			file_assets         = {},
+			show_time           = true,
+			global_timer        = false,
 
-		    editing_text        = "Editing %s",               
-		    file_explorer_text  = "Browsing %s",              
-		    git_commit_text     = "Committing changes",       
-		    plugin_manager_text = "Managing plugins",         
-		    reading_text        = "Reading %s",               
-		    workspace_text      = "Working on %s",            
-		    line_number_text    = "Line %s out of %s",        
-		    terminal_text       = "Using Terminal",           
+			editing_text        = "Editing %s",
+			file_explorer_text  = "Browsing %s",
+			git_commit_text     = "Committing changes",
+			plugin_manager_text = "Managing plugins",
+			reading_text        = "Reading %s",
+			workspace_text      = "Working on %s",
+			line_number_text    = "Line %s out of %s",
+			terminal_text       = "Using Terminal",
 		})
-    end
+	end
 }
