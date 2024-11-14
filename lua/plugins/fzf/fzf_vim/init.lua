3a2eb5aa48f418a8c1b696ac41245f007d265c0f
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-11-14 12:12:32 +0700

    feat: Add new AI model commands to fzf menu

--- lua/plugins/fzf/fzf_vim/init.lua
+++ lua/plugins/fzf/fzf_vim/init.lua
@@ -10,18 +10,50 @@ return {
         -- Добавляем список команд с кастомными названиями
         local commands = {
             {
-                name = "Отправить запрос в Anthropic",
+                name = "Gemini",
+                command = ":AiderSilentSendPromptByCommandline /model openrouter/google/gemini-pro<CR>"
+            },
+            {
+                name = "DeepSeek",
+                command = ":AiderSilentSendPromptByCommandline '/model openrouter/deepseek/deepseek-chat'"
+            },
+            {
+                name = "Llama",
+                command = ":AiderSilentSendPromptByCommandline /model openrouter/anthropic/claude-1.2"
+            },
+            {
+                name = "Claude Instant",
+                command = ":AiderSilentSendPromptByCommandline /model openrouter/anthropic/claude-1.2"
+            },
+            {
+                name = "Claude 3 Haiku",
                 command = ":AiderSilentSendPromptByCommandline /model openrouter/anthropic/claude-3-haiku"
             },
             {
-                name = "Отправить запрос в OpenAI",
+                name = "Claude 3.5 Sonnet",
+                command = ":AiderSilentSendPromptByCommandline /model anthropic/claude-3.5-sonnet"
+            },
+            {
+                name = "GPT-4o",
                 command = ":AiderSilentSendPromptByCommandline /model openrouter/openai/chatgpt-4o-latest"
+            },
+            {
+                name = "GPT-4o-mini",
+                command = ":AiderSilentSendPromptByCommandline /model openrouter/openai/gpt-4o-mini"
+            },
+            {
+                name = "GPT-o1-mini",
+                command = ":AiderSilentSendPromptByCommandline /model openrouter/openai/o1-mini"
+            },
+            {
+                name = "GPT-o1",
+                command = ":AiderSilentSendPromptByCommandline /model openrouter/openai/o1-preview"
             }
         }
 
         vim.keymap.set('n', '§.', function()
             vim.ui.select(commands, {
-                prompt = "Выберите команду:",
+                prompt = "Choose model:",
                 format_item = function(item)
                     return item.name
                 end
